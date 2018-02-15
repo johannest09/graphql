e29148cc378666fe6bbb6decf4e71ebbeba206ca
Johannes Thorleifsson <johannesthorleifsson@Johanness-iMac.lan>
2018-02-15 23:22:51 +0000

    adding schema

--- index.js
+++ index.js
@@ -1,9 +1,28 @@
-import express from 'express';
+import express from 'express'
+import graphqlHTTP from 'express-graphql'
+import schema from './schema'
 
-const app = express();
+const app = express()
 
 app.get('/', (req, res) => {
-    res.send('GraphQL is amazing!');
-});
+    res.send('GraphQL is amazing!')
+})
 
-app.listen(8080, () => console.log('Running server on port localhost:8080/graphql'));
+const root = { friend: () => {
+  return {
+    "id": 92839811,
+    "firstName": "Johannes",
+    "lastName": "Thorleifsson",
+    "gender": "male",
+    "language": "Icelandic",
+    "email": "johannesfreyr@gmail.com"
+  }
+} }
+
+app.use('/graphql', graphqlHTTP({
+  schema,
+  rootValue: root,
+  graphiql: true
+}))
+
+app.listen(8080, () => console.log('Running server on port localhost:8080/graphql'))
