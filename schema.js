86ab455e562b6af382c4d3c7ba575e2314a347c5
Johannes Thorleifsson <johannesthorleifsson@Johanness-iMac.lan>
2018-02-18 15:03:26 +0000

    adding resolvers

--- schema.js
+++ schema.js
@@ -1,17 +1,54 @@
-import { buildSchema } from 'graphql'
+import resolvers from './resolvers'
+import { makeExecutableSchema } from 'graphql-tools'
 
-const schema = buildSchema(`
+const typeDefs = `
   type Friend {
     id: ID
     firstName: String
     lastName: String
-    gender: String
+    gender: Gender
+    age: Int
     language: String
     email: String
+    contacts: [Contact]
   }
-  type Query {
-    friend: Friend
-  }
-`)
 
-export default schema
+  type Contact {
+    firstName: String
+    lastName: String
+
+  }
+
+  enum Gender {
+    MALE
+    FEMALE
+    OTHER
+  }
+
+  type Query {
+    getFriend(id: ID): Friend
+  }
+
+  input FriendInput {
+    id: ID
+    firstName: String!
+    lastName: String
+    gender: Gender
+    age: Int
+    language: String
+    email: String
+    contacts: [ContactInput]
+  }
+
+  input ContactInput {
+    firstName: String
+    lastName: String
+  }
+
+  type Mutation {
+    createFriend(input: FriendInput) : Friend
+  }
+`
+const schema = makeExecutableSchema({ typeDefs, resolvers })
+
+export { schema }
